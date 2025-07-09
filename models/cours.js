da1078ede04edbc0cacd904e27c284b34f6b58f7
louay <louaybou2004@gmail.com>
2025-07-09 10:20:03 +0100

    updates models

--- models/cours.js
+++ models/cours.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../DB/db');
 
-const Product = sequelize.define('Product', {
+const Product = sequelize.define('Cours', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     name: { type: DataTypes.STRING, allowNull: false },
     description: { type: DataTypes.STRING, allowNull: false },
